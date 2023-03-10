937df2fde2e3372417ba1d2db4a10604bcf2a27a
David Rodriguez <juanrodriguez.com58@gmail.com>
2023-03-09 21:58:27 -0600

    Creacion de contructores en las clases Account.php y Car.php y creacion de index.html

--- PHP/Car.php
+++ PHP/Car.php
@@ -4,4 +4,13 @@ class Car{
     public $license;
     public $driver;
     public $passenger;
+
+    public function __construct($license, $driver){
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function PrintDataCar(){
+        echo "License: $this->license, Driver:{$this->driver->name}, Document: {$this->driver->document}";
+    }
 }
