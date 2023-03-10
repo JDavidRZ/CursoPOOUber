4092162514de67231d3dd1064fc1f59f9bb5305a
David Rodriguez <juanrodriguez.com58@gmail.com>
2023-03-09 21:59:11 -0600

    Creacion de contructores en las clases Account.java y Car.java

--- Java/Car.java
+++ Java/Car.java
@@ -1,6 +1,15 @@
 public class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passenger;
+
+    public Car (String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
+
+    public void printDataCar(){
+        System.out.println("Licence: " + license + " Name Driver: " + driver.name);
+    }
 }
