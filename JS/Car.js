29bbed9de41d21e9e6b025f649ec867d3626f2f5
David Rodriguez <juanrodriguez.com58@gmail.com>
2023-03-09 21:56:32 -0600

    Creacion de contructores en las clases Account.js y Car.js y creacion de index.html

--- JS/Car.js
+++ JS/Car.js
@@ -1,6 +1,14 @@
-function Car(){
-    this.id;
-    this.license;
-    this.driver;
-    this.passenger;
+class Car {
+    constructor(license, drive) {
+        this.id;
+        this.license = license;
+        this.driver = drive;
+        this.passenger;
+    }
+
+    printDataCar = function () {
+        console.log(this.driver)
+        console.log(this.driver.name)
+        console.log(this.driver.document)
+    }
 }
